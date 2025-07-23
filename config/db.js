34b0c38aa51fb959fc87fcb6884eeec1e64658a1
Shashank <niteshsrivastava148@gmail.com>
2025-07-23 17:42:17 +0530

    changes in db

--- config/db.js
+++ config/db.js
@@ -1,22 +1,28 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-let cached = global.mongoose
-if(!cached) {
-    cached = global.mongoose  = {conn : null, promise: null}
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
-    if(cached.conn) {
-        return cached.conn
-    }
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    if(!cached.promise) {
-        const opts = {
-            bufferCommands: false
-        }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
 
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI, opts}`)
-    }
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/cartwale`, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+  cached.conn = await cached.promise
+  return cached.conn
 }
 
 export default connectDB;
