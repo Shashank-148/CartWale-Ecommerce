4a0f7f60ebb9f6009869b3397d2f5a0ce7561aac
Shashank <niteshsrivastava148@gmail.com>
2025-07-28 13:17:20 +0530

    changes in code

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -13,6 +15,10 @@ const nextConfig = {
                 pathname: '**',
             },
         ],
+    },
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.resolve(__dirname);
+        return config;
     },
 };
 
